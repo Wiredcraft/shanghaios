f75ac31819cf77532fea8c393377420e9da22dfe
Ronan Berder <ronan@wiredcraft.com>
2013-03-13 12:17:13 +0800

    Good enough for now

--- assets.js
+++ assets.js
@@ -9617,15 +9617,6 @@ $ = require('jquery');
 
 // **Very** simple one liner to illustrate adding JS
 $('a[href="'+ window.location.href +'"]').addClass('active');
-
-// var map = mapbox.auto('map', 'examples.map-vyofok3q');
-
-
-// Create map
-var layer = mapbox.layer().id('tmcw.map-2f4ad161');
-var map = mapbox.map('location', layer, null, []);
-map.centerzoom({ lat: 31.230393 , lon: 121.473704 }, 12);
-
 });
 require.alias("boot/index.js", "devops/deps/boot/index.js");
 
